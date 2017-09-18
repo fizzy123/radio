67e3e8b4da617e9fb6c6cc8eeac5bdd4873e18d0
Nobel Yoo <nobr@nobr.me>
2017-09-18 09:28:08 +0000

    add volume normalization and auto detection of url

--- stream.sh
+++ stream.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # https://stackoverflow.com/questions/43586435/ffmpeg-to-youtube-live
-ffmpeg -loop 1 -re -i $1 -i /tmp/current.wav -vcodec libx264 -vprofile baseline -vf drawtext="fontfile=/path/to/font.ttf: text='$2': fontcolor=white: fontsize=18: box=1: boxcolor=black@0.5: boxborderw=5: x=(w-text_w)/2: y=(h-text_h)/2" -c:a aac -s 1920x1080  -framerate 2 -strict experimental -shortest -preset ultrafast -f flv rtmp://$3/stream
+ffmpeg -loop 1 -re -i $1 -i /tmp/current.wav -af loudnorm=I=-16:TP=-1.5:LRA=11 -vcodec libx264 -vprofile baseline -vf drawtext="fontfile=/path/to/font.ttf: text='$2': fontcolor=white: fontsize=18: box=1: boxcolor=black@0.5: boxborderw=5: x=(w-text_w)/2: y=(h-text_h)/2" -c:a aac -s 1920x1080  -framerate 2 -strict experimental -shortest -preset ultrafast -f flv rtmp://$3/stream
